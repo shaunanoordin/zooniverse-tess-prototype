0bc716c0b6b7fee971e071c154ee9321568e9b63
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2018-07-26 18:02:30 +0100

    Add Plot.ly graph

--- src/containers/TestContainer.jsx
+++ src/containers/TestContainer.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
+
+//Zooniverse Front-End Monorepo
 import { panoptes } from '@zooniverse/panoptes-js';
 import Classifier from '@zooniverse/classifier';
+
+//Plotly Graph
+import Plotly from 'plotly.js-dist';
+import createPlotlyComponent from 'react-plotlyjs';
+const PlotlyComponent = createPlotlyComponent(Plotly);
 
 class TestContainer extends React.Component {
   constructor() {
@@ -31,6 +38,25 @@ class TestContainer extends React.Component {
   render() {
     return (
       <div>
+        <PlotlyComponent
+          data={[
+            {
+              type: 'scatter',  // all "scatter" attributes: https://plot.ly/javascript/reference/#scatter
+              x: [1, 2, 3],     // more about "x": #scatter-x
+              y: [6, 2, 3],     // #scatter-y
+              marker: {         // marker is an object, valid marker keys: #scatter-marker
+                color: 'rgb(16, 32, 77)' // more about "marker.color": #scatter-marker-color
+              }
+            },
+            {
+              type: 'bar',      // all "bar" chart attributes: #bar
+              x: [1, 2, 3],     // more about "x": #bar-x
+              y: [6, 2, 3],     // #bar-y
+              name: 'bar chart example' // #bar-name
+            }
+          ]}
+        />
+        
         <div>Project Status: {this.state.projectStatus}</div>
         {(!this.state.projectData) ? null :
           <Classifier project={this.state.projectData} />
