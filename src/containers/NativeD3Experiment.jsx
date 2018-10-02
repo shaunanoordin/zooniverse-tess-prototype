ab63497bca602bee42eb7e01a85e9dcb63f81b71
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2018-10-02 18:41:11 +0100

    Add native D3 scatterplot

--- src/containers/NativeD3Experiment.jsx
+++ src/containers/NativeD3Experiment.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 //Zooniverse Front-End Monorepo
-import { panoptes } from '@zooniverse/panoptes-js';
+//import { panoptes } from '@zooniverse/panoptes-js';
+
+//Native D3 and a faux-dom
+import * as d3 from 'd3'
+import {withFauxDOM} from 'react-faux-dom'
 
 class NativeD3Experiment extends React.Component {
   constructor() {
@@ -16,20 +20,12 @@ class NativeD3Experiment extends React.Component {
     this.initialise();
   }
   
-  async initialise() {
-    const id = '335';
-    try {
-      this.setState({ projectStatus: 'fetching' });
-      const response = await panoptes.get(`/projects/${id}`);
-      const project = response.body.projects[0];
-      this.setState({ projectData: project, projectStatus: 'success' });
-    } catch (error) {
-      this.setState({ projectStatus: 'error' });
-      console.error(`Error fetching project ${id}`, error);
-    }
-  }
-
-  render() {
+  initialise() {
+    const props = this.props;
+    const state = this.state;
+    
+    //Generate test data
+    //--------------------------------
     const r = (n = 100) => { return Math.floor(Math.random()*n) }
     const t = (x) => {
       return '' +
@@ -44,10 +40,40 @@ class NativeD3Experiment extends React.Component {
         label: t(i),
       });
     }
+    //--------------------------------
+    
+    //Genrate the faux node
+    //--------------------------------
+    const fauxNode = this.props.connectFauxDOM('svg', 'chartNode')
+    const dataLayer = d3.select(fauxNode)
+      .attr('width', props.width)
+      .attr('height', props.height)
+      .attr('class', 'd3-chart')
+        .append('g')
+        .attr('class', 'data-layer');
+  
+    dataLayer.selectAll('circle')
+      .data(testData)
+      .enter()
+        .append('circle')
+        .attr('class', 'data-point')
+        .attr('cx', (d) => d.x)
+        .attr('cy', (d) => d.y)
+        .attr('r', (d) => 1)
+    ;
+    
+    
+    this.props.animateFauxDOM(800);
+    //--------------------------------
+  }
+
+  render() {
+    const props = this.props;
+    const state = this.state;
     
     return (
-      <div>
-        <div>Project Status: {this.state.projectStatus}</div>
+      <div className="d3-chart">
+        {props.chartNode}
       </div>
     );
   } 
@@ -57,7 +83,14 @@ class NativeD3Experiment extends React.Component {
 --------------------------------------------------------------------------------
  */
 
-NativeD3Experiment.defaultProps = {};
-NativeD3Experiment.propTypes = {};
+NativeD3Experiment.defaultProps = {
+  chartNode: 'Loading...',
+  width: 200,
+  height: 200,
+};
 
-export default NativeD3Experiment;
+NativeD3Experiment.propTypes = {
+  //TODO
+};
+
+export default withFauxDOM(NativeD3Experiment);
