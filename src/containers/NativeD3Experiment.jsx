f219378de34fd681ee6df93feae6f8689ef5aed2
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2018-10-03 17:37:08 +0100

    Going mad trying to add interactive pieces to the Native D3 implementation

--- src/containers/NativeD3Experiment.jsx
+++ src/containers/NativeD3Experiment.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 //Zooniverse Front-End Monorepo
 //import { panoptes } from '@zooniverse/panoptes-js';
 
 //Native D3 and a faux-dom
-import * as d3 from 'd3'
-import {withFauxDOM} from 'react-faux-dom'
+import * as d3 from 'd3';
+import * as d3Zoom from 'd3-zoom';
+import {withFauxDOM} from 'react-faux-dom';
 
 class NativeD3Experiment extends React.Component {
   constructor() {
@@ -26,33 +28,34 @@ class NativeD3Experiment extends React.Component {
     
     //Generate test data
     //--------------------------------
-    const r = (n = 100) => { return Math.floor(Math.random()*n) }
-    const t = (x) => {
-      return '' +
-        ' ten twenty thirty forty fifty sixty seventy eighty ninety hundred ???'.split(' ')[Math.floor(x/10)] + ' ' +
-        ' one two three four five six seven eight nine ???'.split(' ')[Math.floor(x%10)];
-    };
     let testData = [];
-    for (let i = 1; i <= 100; i++) {
+    const rY = (x) => {
+      return props.height - Math.random() * Math.abs(props.width-x);
+    };
+    for (let i = 1; i <= props.width; i++) {
       testData.push({
         x: i,
-        y: r(),
-        label: t(i),
+        y: rY(i),
       });
     }
     //--------------------------------
     
-    //Genrate the faux node
+    //Genrate the SVG on the faux node
     //--------------------------------
-    const fauxNode = this.props.connectFauxDOM('svg', 'chartNode')
-    const dataLayer = d3.select(fauxNode)
+    const fauxNode = this.props.connectFauxDOM('svg', 'chartNode');
+    
+    const d3Svg = d3.select(fauxNode)
+      .attr('viewBox', `0 0 ${props.width} ${props.height}`)
+      .attr('class', 'd3-svg')
       .attr('width', props.width)
       .attr('height', props.height)
-      .attr('class', 'd3-chart')
-        .append('g')
-        .attr('class', 'data-layer');
+    ;
+    
+    const d3DataLayer = d3Svg.append('g')
+      .attr('class', 'data-layer')
+    ;
   
-    dataLayer.selectAll('circle')
+    d3DataLayer.selectAll('circle')
       .data(testData)
       .enter()
         .append('circle')
@@ -62,8 +65,16 @@ class NativeD3Experiment extends React.Component {
         .attr('r', (d) => 1)
     ;
     
+    const d3InterfaceLayer = d3Svg.append('rect')
+      .attr('class', 'interface-layer')
+      .attr('width', props.width)
+      .attr('height', props.height)
+      .style('fill', 'none')
+      .style('pointer-events', 'all')
+    ;
     
-    this.props.animateFauxDOM(800);
+    //TODO: now what is this for?
+    //this.props.animateFauxDOM(800);
     //--------------------------------
   }
 
@@ -72,7 +83,7 @@ class NativeD3Experiment extends React.Component {
     const state = this.state;
     
     return (
-      <div className="d3-chart">
+      <div className="native-d3-chart">
         {props.chartNode}
       </div>
     );
@@ -85,8 +96,8 @@ class NativeD3Experiment extends React.Component {
 
 NativeD3Experiment.defaultProps = {
   chartNode: 'Loading...',
-  width: 200,
-  height: 200,
+  width: 500,
+  height: 500,
 };
 
 NativeD3Experiment.propTypes = {
